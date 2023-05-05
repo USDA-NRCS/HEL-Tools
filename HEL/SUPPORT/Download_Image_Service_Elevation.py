a00e5640dc58ac1da0c799cb15d5866431a6dcab
Dylan Harwell <dylan.harwell@usda.gov>
2023-05-04 17:27:02 -0700

    Download DEM tool validation and error handling

--- HEL/SUPPORT/Download_Image_Service_Elevation.py
+++ HEL/SUPPORT/Download_Image_Service_Elevation.py
@@ -1,72 +1,76 @@
-from arcpy import AddError, AddMessage, CheckExtension, CheckOutExtension, Describe, env, Exists, \
-    GetParameterAsText, SetParameterAsText, SpatialReference
+from arcpy import AddError, AddMessage, Describe, env, Exists, GetParameterAsText, SetParameterAsText, SpatialReference
 from arcpy.analysis import Buffer
 from arcpy.management import Clip, Delete, Project, ProjectRaster
 
 from os import path
-from sys import argv, exit
+from sys import argv
 
-
-# Check out Spatial Analyst License
-if CheckExtension('Spatial') == 'Available':
-    CheckOutExtension('Spatial')
-else:
-    AddError('Spatial Analyst Extension must be enabled to use this tool... Exiting')
-    exit()
-
-# Geoprocessing Environment Settings
-scratch_gdb = path.join(path.dirname(argv[0]), 'SCRATCH.gdb')
-env.scratchWorkspace = scratch_gdb
-env.overwriteOutput = True
-env.geographicTransformations = 'WGS_1984_(ITRF00)_To_NAD_1983'
-env.resamplingMethod = 'BILINEAR'
-env.pyramid = 'PYRAMIDS -1 BILINEAR DEFAULT 75 NO_SKIP'
 
 # Tool Inputs
 source_clu = GetParameterAsText(0)
 source_service = GetParameterAsText(1)
 
-# Variables
+# Paths to SCRATCH.gdb features
+scratch_gdb = path.join(path.dirname(argv[0]), 'SCRATCH.gdb')
+clu_buffer = path.join(scratch_gdb, 'CLU_Buffer')
+wgs84_clu_buffer = path.join(scratch_gdb, 'WGS84_CLU_Buffer')
 wgs84_DEM = path.join(scratch_gdb, 'WGS84_DEM')
 final_DEM = path.join(scratch_gdb, 'Downloaded_DEM')
-clu_buffer = path.join(scratch_gdb, 'CLU_Buffer')
-clu_buff_wgs = path.join(scratch_gdb, 'CLU_Buffer_WGS')
 
-# Delete posible temp datasets if they already exist
-if Exists(wgs84_DEM):
-    Delete(wgs84_DEM)
-if Exists(final_DEM):
-    Delete(final_DEM)
-if Exists(clu_buff_wgs):
-    Delete(clu_buff_wgs)
+# Geoprocessing Environment Settings
+env.workspace = scratch_gdb
+env.overwriteOutput = True
+env.geographicTransformations = 'WGS_1984_(ITRF00)_To_NAD_1983'
+env.resamplingMethod = 'BILINEAR'
+env.pyramid = 'PYRAMIDS -1 BILINEAR DEFAULT 75 NO_SKIP'
 
-# Buffer the selected CLUs by 400m
+# Clear SCRATCH.gdb
+scratch_features = [clu_buffer, wgs84_clu_buffer, wgs84_DEM, final_DEM]
+for feature in scratch_features:
+    if Exists(feature):
+        Delete(feature)
+
+# Buffer the selected CLUs
 AddMessage('Buffering input CLU fields...')
-# Use 410 meter radius so that you have a bit of extra area for the HEL Determination tool to clip against
-# This distance is designed to minimize problems of no data crashes if the HEL Determiation tool's resampled 3-meter DEM doesn't perfectly snap with results from this tool.
-Buffer(source_clu, clu_buffer, '410 Meters', 'FULL', '', 'ALL', '')
+try:
+    Buffer(source_clu, clu_buffer, '410 Meters', 'FULL', '', 'ALL', '')
+except Exception:
+    AddError('Failed to buffer selected CLU(s)... Exiting')
+    exit()
 
-# Re-project the AOI to WGS84 Geographic (EPSG WKID: 4326)
-AddMessage('Converting CLU Buffer to WGS 1984...')
-Project(clu_buffer, clu_buff_wgs, SpatialReference(4326))
+# Re-project the CLU Buffer to WGS84 Geographic (EPSG WKID: 4326)
+AddMessage('Projecting CLU Buffer to WGS 1984...')
+try:
+    Project(clu_buffer, wgs84_clu_buffer, SpatialReference(4326))
+except Exception:
+    AddError('Failed to project CLU Buffer to WGS84... Exiting')
+    exit()
 
-# Use the WGS 1984 AOI to clip/extract the DEM from the service
-AddMessage('Downloading Data...')
-aoi_ext = Describe(clu_buff_wgs).extent
-clip_ext = f"{str(aoi_ext.XMin)} {str(aoi_ext.YMin)} {str(aoi_ext.XMax)} {str(aoi_ext.YMax)}"
-Clip(source_service, clip_ext, wgs84_DEM, '', '', '', 'NO_MAINTAIN_EXTENT')
+# Use the WGS84 CLU Buffer to clip DEM service
+AddMessage('Clipping DEM service to area of interest...')
+try:
+    aoi_ext = Describe(wgs84_clu_buffer).extent
+    clip_ext = f"{str(aoi_ext.XMin)} {str(aoi_ext.YMin)} {str(aoi_ext.XMax)} {str(aoi_ext.YMax)}"
+    Clip(source_service, clip_ext, wgs84_DEM, '', '', '', 'NO_MAINTAIN_EXTENT')
+except Exception:
+    AddError('Failed to clip DEM service to area of interest... Exiting')
+    exit()
 
-# Project the WGS 1984 DEM to the coordinate system of the input CLU layer
-AddMessage('Projecting data to match input CLU...')
-final_CS = Describe(source_clu).spatialReference.factoryCode
-ProjectRaster(wgs84_DEM, final_DEM, final_CS, 'BILINEAR', 3)
+# Project the WGS84 DEM to the coordinate system of the input CLU layer
+AddMessage('Projecting DEM to match input CLU...')
+try:
+    final_CS = Describe(source_clu).spatialReference.factoryCode
+    ProjectRaster(wgs84_DEM, final_DEM, final_CS, 'BILINEAR', 3)
+except Exception:
+    AddError('Failed to project clipped DEM to CLU coordinate system... Exiting')
+    exit()
 
-# Delete temporary data
-AddMessage('Cleaning up...')
-Delete(wgs84_DEM)
-Delete(clu_buff_wgs)
-Delete(clu_buffer)
+# Clear SCRATCH.gdb except final DEM
+scratch_features = [clu_buffer, wgs84_clu_buffer, wgs84_DEM]
+for feature in scratch_features:
+    if Exists(feature):
+        Delete(feature)
 
-# Add resulting data to map
-AddMessage('Adding layer to map...')
+# Add final DEM layer to derived output parameter
+AddMessage('Adding DEM layer to map...')
 SetParameterAsText(2, final_DEM)
