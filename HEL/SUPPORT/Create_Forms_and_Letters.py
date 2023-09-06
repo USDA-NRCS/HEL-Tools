e7fa23efa3a8b3ed7e61a88ed17a8213ccbd3f48
Harwell <12001003312449@FEDIDCARD.GOV>
2023-09-06 15:23:38 -0700

    WIP: forms and letters tool params and validation, script modifications for new params

--- HEL/SUPPORT/Create_Forms_and_Letters.py
+++ HEL/SUPPORT/Create_Forms_and_Letters.py
@@ -27,7 +27,7 @@ def add_blank_rows(table_data, max_rows):
 ### Initial Tool Validation ###
 try:
     aprx = ArcGISProject('CURRENT')
-    aprx.listMaps('Determinations')[0]
+    aprx.listMaps('HEL Determination')[0]
 except Exception:
     AddError('This tool must be run from an ArcGIS Pro project that was developed from the template distributed with this toolbox. Exiting...')
     exit()
@@ -36,28 +36,20 @@ AddMessage('Collecting inputs...')
 SetProgressorLabel('Collecting inputs...')
 
 ### Input Parameters ###
-site_cwd_lyr = GetParameterAsText(0)
-include_pjw_statement = GetParameterAsText(1)
-create_028_form = GetParameterAsText(2)
+field_det_lyr = GetParameterAsText(0)
+hel_map_units = GetParameterAsText(1)
+where_completed = GetParameterAsText(2)
 nrcs_office = GetParameterAsText(3)
 fsa_county = GetParameterAsText(4)
 fsa_office = GetParameterAsText(5)
-consolidate_by_wetland = GetParameterAsText(6)
 
 AddMessage('Assigning local variables...')
 SetProgressorLabel('Assigning local variables...')
 
 ### Paths to Word Templates ###
 templates_dir = os_path.join(base_dir, 'Templates')
-customer_letter_template_path = os_path.join(templates_dir, 'WC_Letter_Template.docx')
-cpa_026_wc_template_path = os_path.join(templates_dir, 'CPA_026_WC_Template.docx')
-cpa_026_wc_supplemental_template_path = os_path.join(templates_dir, 'SupplementalWorksheet_026.docx')
-cpa_026_wc_supplemental_template_output = os_path.join(templates_dir, 'SupplementalWorksheet_026_TEMP.docx')
-cpa_026_wc_definitions_template_path = os_path.join(templates_dir, 'DefinitionsNDS_026.docx')
-cpa_028_wc_template_path = os_path.join(templates_dir, 'CPA_028_WC_Template.docx')
-cpa_028_wc_supplemental_template_path = os_path.join(templates_dir, 'SupplementalWorksheet_028.docx')
-cpa_028_wc_supplemental_template_output = os_path.join(templates_dir, 'SupplementalWorksheet_028_TEMP.docx')
-cpa_028_wc_definitions_template_path = os_path.join(templates_dir, 'DefinitionsNDS_028.docx')
+customer_letter_template_path = os_path.join(templates_dir, 'HELC_Letter_Template.docx')
+cpa_026_helc_template_path = os_path.join(templates_dir, 'CPA_026_HELC_Template.docx')
 
 ### Paths to SUPPORT GDB ###
 support_gdb = os_path.join(base_dir, 'SUPPORT.gdb')
@@ -66,24 +58,19 @@ fsa_addresses_table = os_path.join(support_gdb, 'fsa_addresses')
 nad_addresses_table = os_path.join(support_gdb, 'nad_addresses')
 
 ### Paths to Site GDB ###
-site_cwd_lyr_path = Describe(site_cwd_lyr).CatalogPath
-site_gdb = site_cwd_lyr_path[:site_cwd_lyr_path.find('.gdb')+4]
+field_det_lyr_path = Describe(field_det_lyr).CatalogPath
+site_gdb = field_det_lyr_path[:field_det_lyr_path.find('.gdb')+4]
 admin_table = os_path.join(site_gdb, 'Admin_Table')
-clu_cwd_026_table = os_path.join(site_gdb, 'CLU_CWD_026')
-clu_cwd_026_alt_table = os_path.join(site_gdb, 'CLU_CWD_026_alt')
-clu_cwd_028_table = os_path.join(site_gdb, 'CLU_CWD_028')
 
-### Paths to Site Wetlands Folder for Outputs ###
-wetlands_dir = os_path.dirname(site_gdb)
-customer_letter_output = os_path.join(wetlands_dir, 'WC_Letter.docx')
-cpa_026_wc_output = os_path.join(wetlands_dir, 'NRCS-CPA-026-WC-Form.docx')
-cpa_028_wc_output = os_path.join(wetlands_dir, 'NRCS-CPA-028-WC-Form.docx')
+### Paths to HEL Project Folder for Outputs ###
+hel_dir = os_path.dirname(site_gdb)
+customer_letter_output = os_path.join(hel_dir, 'HELC_Letter.docx')
+cpa_026_helc_output = os_path.join(hel_dir, 'NRCS-CPA-026-HELC-Form.docx')
 
 
 ### Ensure Word Doc Templates Exist ###
 #NOTE: Front end validation checks the existance of most geodatabase tables
-for path in [customer_letter_template_path, cpa_026_wc_template_path, cpa_026_wc_supplemental_template_path, cpa_026_wc_definitions_template_path,
-    cpa_028_wc_template_path, cpa_028_wc_supplemental_template_path, cpa_028_wc_definitions_template_path]:
+for path in [customer_letter_template_path, cpa_026_helc_template_path]:
     if not os_path.exists(path):
         AddError(f'Error: Failed to locate required Word template: {path}. Exiting...')
         exit()
@@ -213,7 +200,7 @@ AddMessage('Retrieved data from NAD Addresses Table...')
 
 
 ### Generate Customer Letter ###
-SetProgressorLabel('Generating WC_Letter.docx...')
+SetProgressorLabel('Generating HELC_Letter.docx...')
 try:
     customer_letter_template = DocxTemplate(customer_letter_template_path)
     context = {
@@ -226,39 +213,37 @@ try:
     }
     customer_letter_template.render(context, autoescape=True)
     customer_letter_template.save(customer_letter_output)
-    AddMessage('Created WC_Letter.docx...')
+    AddMessage('Created HELC_Letter.docx...')
 except PermissionError:
     AddError('Error: Please close any open Word documents and try again. Exiting...')
     exit()
 except Exception as e:
-    AddError('Error: Failed to create WC_Letter.docx. Exiting...')
+    AddError('Error: Failed to create HELC_Letter.docx. Exiting...')
     AddError(e)
     exit()
 
 
-### Read and assign values from CLU CWD 026 Table ###
-SetProgressorLabel('Generating NRCS-CPA-026-WC-Form.docx...')
+### Read and assign values from Field Determination feature class ###
+SetProgressorLabel('Generating NRCS-CPA-026-HELC-Form.docx...')
 try:
-    table_026 = clu_cwd_026_alt_table if consolidate_by_wetland == 'true' else clu_cwd_026_table
-    data_026_pg1 = [] #11 rows
-    data_026_pg2 = [] #15 rows
-    data_026_extra = []
-    fields = ['clu_number', 'wetland_label', 'occur_year', 'SUM_acres']
-    with SearchCursor(table_026, fields) as cursor:
+    data_026_pg1 = [] #18 rows
+    # data_026_pg2 = [] #15 rows #TODO: determine how to handle more than 18 rows of data?
+    # data_026_extra = []
+    fields = ['clu_number', 'HEL_YES', 'clu_calculated_acreage']
+    with SearchCursor(field_det_lyr, fields) as cursor:
         row_count = 0
         for row in cursor:
             row_count += 1
             row_data = {}
             row_data['clu'] = row[0] if row[0] else ''
-            row_data['label'] = row[1] if row[1] else ''
-            row_data['year'] = row[2] if row[2] else ''
-            row_data['acres'] = f'{row[3]:.2f}' if row[3] else ''
-            if row_count < 12:
+            row_data['hel_nhel'] = row[1] if row[1] else ''
+            row_data['acres'] = f'{row[2]:.2f}' if row[2] else ''
+            if row_count < 19:
                 data_026_pg1.append(row_data)
-            elif row_count > 11 and row_count < 27:
-                data_026_pg2.append(row_data)
-            else:
-                data_026_extra.append(row_data)
+            # elif row_count > 11 and row_count < 27:
+            #     data_026_pg2.append(row_data)
+            # else:
+            #     data_026_extra.append(row_data)
 except Exception as e:
     AddError('Error: failed while retrieving CLU CWD 026 Table data. Exiting...')
     AddError(e)
@@ -266,168 +251,58 @@ except Exception as e:
 
 
 ### Generate Pages 1 and 2 of 026 Form ###
-pjw_statement = 'Areas identified as Potential Jurisdictional Waters (PJW) are not subject to the Food Security Act but are potentially subject to the Clean Water Act. You should contact the U.S. Army Corps of Engineers local district office prior to performing work in areas which may be subject to the Clean Water Act. Areas identified as PJW are for informational purposes only and are not appealable to USDA. Additional areas, not identified by NRCS, might also be subject to the Clean Water Act.'
 try:
-    cpa_026_wc_template = DocxTemplate(cpa_026_wc_template_path)
+    cpa_026_helc_template = DocxTemplate(cpa_026_helc_template_path)
     context = {
         'admin_data': admin_data,
-        'pjw_statement': pjw_statement if include_pjw_statement == 'Yes' else '',
-        'data_026_pg1': add_blank_rows(data_026_pg1, 11),
-        'data_026_pg2': add_blank_rows(data_026_pg2, 15) 
+        'hel_map_units': hel_map_units,
+        'where_completed': where_completed,
+        'data_026_pg1': add_blank_rows(data_026_pg1, 18)
     }
-    cpa_026_wc_template.render(context, autoescape=True)
-    cpa_026_wc_template.save(cpa_026_wc_output)
-    cpa_026_wc_doc = Document(cpa_026_wc_output)
-    cpa_026_wc_composer = Composer(cpa_026_wc_doc)
-    AddMessage('Created pages 1 and 2 of NRCS-CPA-026-WC-Form.docx...')
+    cpa_026_helc_template.render(context, autoescape=True)
+    cpa_026_helc_template.save(cpa_026_helc_output)
+    cpa_026_helc_doc = Document(cpa_026_helc_output)
+    cpa_026_helc_composer = Composer(cpa_026_helc_doc)
+    AddMessage('Created pages 1 and 2 of NRCS-CPA-026-HELC-Form.docx...')
 except PermissionError:
     AddError('Error: Please close any open Word documents and try again. Exiting...')
     exit()
 except Exception as e:
-    AddError('Error: Failed to create pages 1 and 2 of NRCS-CPA-026-WC-Form.docx. Exiting...')
+    AddError('Error: Failed to create pages 1 and 2 of NRCS-CPA-026-HELC-Form.docx. Exiting...')
     AddError(e)
     exit()
 
 
-### Generate 026 Supplemental Pages if Needed ###
-if data_026_extra:
-    number_pages = ceil(len(data_026_extra)/15)
-    for page_number in range(number_pages):
-        # Fill pages with sets of 15, removing from original list
-        page_data = []
-        page_data.extend(data_026_extra[:15])
-        del data_026_extra[:15]
-        try:
-            page_data = add_blank_rows(page_data, 15)
-            cpa_026_wc_supplemental_template = DocxTemplate(cpa_026_wc_supplemental_template_path)
-            context = {'data_026_extra': page_data}
-            cpa_026_wc_supplemental_template.render(context, autoescape=True)
-            cpa_026_wc_supplemental_template.save(cpa_026_wc_supplemental_template_output)
-            supplemental_026_doc = Document(cpa_026_wc_supplemental_template_output)
-            cpa_026_wc_composer.append(supplemental_026_doc)
-            remove(cpa_026_wc_supplemental_template_output)
-            AddMessage(f'Created 026 Supplemental Worksheet page {page_number+3}...')
-        except Exception as e:
-            AddError(f'Error: Failed to create 026 Supplemental Worksheet page {page_number+3}. Exiting...')
-            AddError(e)
-            exit()
+# ### Generate 026 Supplemental Pages if Needed ###
+# if data_026_extra:
+#     number_pages = ceil(len(data_026_extra)/15)
+#     for page_number in range(number_pages):
+#         # Fill pages with sets of 15, removing from original list
+#         page_data = []
+#         page_data.extend(data_026_extra[:15])
+#         del data_026_extra[:15]
+#         try:
+#             page_data = add_blank_rows(page_data, 15)
+#             cpa_026_helc_supplemental_template = DocxTemplate(cpa_026_helc_supplemental_template_path)
+#             context = {'data_026_extra': page_data}
+#             cpa_026_helc_supplemental_template.render(context, autoescape=True)
+#             cpa_026_helc_supplemental_template.save(cpa_026_helc_supplemental_template_output)
+#             supplemental_026_doc = Document(cpa_026_helc_supplemental_template_output)
+#             cpa_026_helc_composer.append(supplemental_026_doc)
+#             remove(cpa_026_helc_supplemental_template_output)
+#             AddMessage(f'Created 026 Supplemental Worksheet page {page_number+3}...')
+#         except Exception as e:
+#             AddError(f'Error: Failed to create 026 Supplemental Worksheet page {page_number+3}. Exiting...')
+#             AddError(e)
+#             exit()
 
 
-### Combine Pages and Save Final 026 Form ###
-try:
-    definitions_026_doc = Document(cpa_026_wc_definitions_template_path)
-    cpa_026_wc_composer.append(definitions_026_doc)
-    cpa_026_wc_composer.save(cpa_026_wc_output)
-    AddMessage('Appended Wetland Definitions template and finalized NRCS-CPA-026-WC-Form.docx...')
-except PermissionError:
-    AddError('Error: Please close any open Word documents and try again. Exiting...')
-    exit()
-except Exception as e:
-    AddError('Error: Failed to append Wetland Definitions template and finalize 026 Form. Exiting...')
-    AddError(e)
-    exit()
-
-
-### Read and assign values from CLU CWD 028 Table ###
-if create_028_form == 'Yes':
-    try:
-        SetProgressorLabel('Generating NRCS-CPA-028-WC-Form.docx...')
-        data_028_pg1 = [] #12 rows
-        data_028_pg2 = [] #20 rows
-        data_028_extra = []
-        fields = ['clu_number', 'wetland_label', 'occur_year', 'cert_date', 'SUM_acres']
-        with SearchCursor(clu_cwd_028_table, fields) as cursor:
-            row_count = 0
-            for row in cursor:
-                row_count += 1
-                row_data = {}
-                row_data['clu'] = row[0] if row[0] else ''
-                row_data['label'] = row[1] if row[1] else ''
-                row_data['year'] = row[2] if row[2] else ''
-                row_data['date'] = row[3].strftime('%m/%d/%Y') if row[3] else ''
-                row_data['acres'] = f'{row[4]:.2f}' if row[4] else ''
-                if row_count < 13:
-                    data_028_pg1.append(row_data)
-                elif row_count > 12 and row_count < 33:
-                    data_028_pg2.append(row_data)
-                else:
-                    data_028_extra.append(row_data)
-    except Exception as e:
-        AddError('Error: failed while retrieving CLU CWD 028 Table data. Exiting...')
-        AddError(e)
-        exit()
-
-
-    ### Generate Pages 1 and 2 of 028 Form ###
-    try:
-        cpa_028_wc_template = DocxTemplate(cpa_028_wc_template_path)
-        context = {
-            'today_date': date.today().strftime('%m/%d/%Y'),
-            'admin_data': admin_data,
-            'data_028_pg1': add_blank_rows(data_028_pg1, 12),
-            'data_028_pg2': add_blank_rows(data_028_pg2, 20) 
-        }
-        cpa_028_wc_template.render(context, autoescape=True)
-        cpa_028_wc_template.save(cpa_028_wc_output)
-        cpa_028_wc_doc = Document(cpa_028_wc_output)
-        cpa_028_wc_composer = Composer(cpa_028_wc_doc)
-        AddMessage('Created pages 1 and 2 of NRCS-CPA-028-WC-Form.docx...')
-    except PermissionError:
-        AddError('Error: Please close any open Word documents and try again. Exiting...')
-        exit()
-    except Exception as e:
-        AddError('Error: Failed to create pages 1 and 2 of NRCS-CPA-028-WC-Form.docx. Exiting...')
-        AddError(e)
-        exit()
-
-
-    ### Generate 028 Supplemental Pages if Needed ###
-    if data_028_extra:
-        number_pages = ceil(len(data_028_extra)/20)
-        for page_number in range(number_pages):
-            # Fill pages with sets of 20, removing from original list
-            page_data = []
-            page_data.extend(data_028_extra[:20])
-            del data_028_extra[:20]
-            try:
-                page_data = add_blank_rows(page_data, 20)
-                cpa_028_wc_supplemental_template = DocxTemplate(cpa_028_wc_supplemental_template_path)
-                context = {'data_028_extra': page_data}
-                cpa_028_wc_supplemental_template.render(context, autoescape=True)
-                cpa_028_wc_supplemental_template.save(cpa_028_wc_supplemental_template_output)
-                supplemental_028_doc = Document(cpa_028_wc_supplemental_template_output)
-                cpa_028_wc_composer.append(supplemental_028_doc)
-                remove(cpa_028_wc_supplemental_template_output)
-                AddMessage(f'Created 028 Supplemental Worksheet page {page_number+3}...')
-            except Exception as e:
-                AddError(f'Error: Failed to create 028 Supplemental Worksheet page {page_number+3}. Exiting...')
-                AddError(e)
-                exit()
-
-    
-    ### Combine Pages and Save Final 028 Form ###
-    try:
-        definitions_028_doc = Document(cpa_028_wc_definitions_template_path)
-        cpa_028_wc_composer.append(definitions_028_doc)
-        cpa_028_wc_composer.save(cpa_028_wc_output)
-        AddMessage('Combined pages and created NRCS-CPA-028-WC-Form.docx...')
-    except PermissionError:
-        AddError('Error: Please close any open Word documents and try again. Exiting...')
-        exit()
-    except Exception as e:
-        AddError('Error: Failed to append Wetland Definitions template and finalize 028 Form. Exiting...')
-        AddError(e)
-        exit()
-
-
-### Open Customer Letter, 026 Form, and 028 Form (Optional) ###
+### Open Customer Letter, 026 Form ###
 AddMessage('Finished generating forms, opening in Microsoft Word...')
 SetProgressorLabel('Finished generating forms, opening in Microsoft Word...')
 try:
     startfile(customer_letter_output)
-    startfile(cpa_026_wc_output)
-    if create_028_form == 'Yes':
-        startfile(cpa_028_wc_output)
+    startfile(cpa_026_helc_output)
 except Exception as e:
     AddError('Error: Failed to open finished forms in Microsoft Word. End of script.')
     AddError(e)
